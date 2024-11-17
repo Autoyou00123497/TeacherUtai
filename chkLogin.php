077da0bfa4c55a8297eeb67646e003ec85c1f6c5
Auto <auto@dekd.com>
2024-11-17 10:48:51 +0700

    update login

--- chkLogin.php
+++ chkLogin.php
@@ -11,10 +11,32 @@ if(isset($_POST['Username'])){
         $row=mysqli_fetch_array($result);
         $_SESSION["UserID"]=$row["u_id"];
         $_SESSION["Name"]=$row["u_fname"]."  ".$row["u_lname"];
-        
+        $_SESSION["img"]=$row["u_img"];
+        $_SESSION["sex"]=$row["u_sex"];
+        $_SESSION["status"]=$row["u_status"];
 
+        if($_SESSION["status"]=="A"){
+        header("location: index_a.php");
 
-    }
+        }
+        if($_SESSION["status"]=="M"){
+            header("location: index_m.php");
+    
+            }
+    
+
+    }else {
+
+        echo"<script>";
+        echo"alert(\"Username or Paswword incorret\");";
+        echo "Window.history.back()";
+        echo "</script>";
+    } 
+
+}else {
+
+    header("location: index.php");
+    
 }
 
 
