5d10219c3c843a45eec2e6225b82525b1a3bb8bb
Auto <auto@dekd.com>
2024-11-17 09:57:16 +0700

    Add login teacher utai

---
+++ Loginutai.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="" href="login.css">
+    <title>Login User</title>
+</head>
+<body>
+    <section>
+        <div>
+
+        <div class="form-box">
+
+        <div class="form-value">
+
+        <from action ="">
+
+        <h2>Login </h2>
+        <div class="inputbox">
+        <ion-icon name="lock-closed-outline">   </ion-icon>      
+        <ion-icon name="mail-outline">   </ion-icon>      
+            <input type = "text" required>
+            <label for="">User</label>
+
+        </div>
+
+       
+
+        <div class="inputbox">
+        <ion-icon name="lock-closed-outline">   </ion-icon>      
+        <ion-icon name="mail-outline">   </ion-icon>      
+            <input type = "password" required>
+            <label for="">Password</label>
+
+        </div>
+
+        <div class="forget">
+
+        <label for=""><input type="checkbox" value="">Remember Me <a href="#">Forget password</a></label>
+
+        </div>
+        <button type="button">login</button>
+        <div class="register">
+        <p>Don't have a account <a href="#">Register</a></p>
+        </div>
+        </from>
+
+
+        </div>
+    </section>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<link>
+</body>
+</html>
